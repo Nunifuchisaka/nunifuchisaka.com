acf9f761faf77906044dc8e18e20b25737b4d555
Nunifuchisaka <nii.splatoon@gmail.com>
2015-11-13 18:15:35 +0900

    .52ガロンを眺めるイカちゃん

--- www/htdocs/index.php
+++ www/htdocs/index.php
@@ -8,7 +8,7 @@
 
 <!-- MEMO：イカやりたい -->
 
-<title>ぬにふちさかのホームページ</title>
+<title>日刊ぬにふちさか</title>
 <meta name="description" content="自己紹介とか日記を書いていこうと思います。">
 <meta name="keywords" content="Splatoon">
 <meta name="author" content="nii.splatoon@gmail.com">
@@ -24,7 +24,7 @@
 -->
 
 <!-- stylesheet -->
-<link rel="stylesheet" href="/assets/css/common.css">
+<link rel="stylesheet" href="/assets/css/common.css?v=2">
 <link rel="stylesheet" href="/assets/css/utility.css">
 <link rel="stylesheet" href="/assets/css/patch.css">
 
@@ -49,13 +49,43 @@ require_once('assets/html/header.php');
 
 <header class="l-header-1">
   <h1 class="l-header-1__h1">
-    <a href="/">ぬにふちさかのホームページ</a>
+    <a href="/">日刊ぬにふちさか</a>
   </h1>
 </header>
 
 
 <div class="l-container-1">
   <div class="l-container-1__primary">
+    
+    <article class="article-1">
+      <header class="header-1">
+        <h1 class="header-1__h1">.52ガロンを眺めるイカちゃん</h1>
+        <p class="header-1__date">2015.11.13</p>
+      </header>
+      <div class="article-1__contents">
+        <p>くそかわ</p>
+        <figure>
+          <img src="img/20151113-1.jpeg" alt=".52ガロンを眺めるイカちゃん" width="800" height="450">
+        </figure>
+        <figure>
+          <img src="img/20151113-2.jpeg" alt=".52ガロンを眺めるイカちゃん（アップ）" width="180" height="260">
+          <figcaption>「・・・（.52ガロンかわいい）」</figcaption>
+        </figure>
+      </div>
+    </article>
+    
+    <article class="article-1">
+      <header class="header-1">
+        <h1 class="header-1__h1">ボールドマーカーネオ所感</h1>
+        <p class="header-1__date">2015.11.12</p>
+      </header>
+      <div class="article-1__contents">
+        <p>念願のボールドマーカーネオ超嬉しい。無印よりネオのほうが僕は好き。ひたすらポイセン投げまくってプレッシャーかけていく形が面白いかなと思う。</p>
+        <p>ギアはイカ速度16、インク回復9、マキガ、安全靴でS+40くらいから連戦してS90くらいで終了。基本僕もボロボロだったけど、僕のキルレが２くらいで味方が奮わず、といったことが試合も多かった。いつもの.52ガロンの立ち回りと比べて味方に負担をかけてしまっていたのかもしれない。裏取りもいつもよりやったし。正面から撃ち合えないっていうのはきついね。</p>
+        <p>ダイオウは普通に強かった。特にイカ速度乗ってるからサクっと複数杯持っていけることが多かった。逆に速すぎて空振りしまくって恥ずかしかったけど慣れれば大丈夫…！</p>
+        <p>次はギアをスペシャル減少10、イカ速度27、スペシャル増加10、安全靴で試してみたい。マキガなくて通用するかな？と思うけどスペシャルをガンガン使っていく形のほうが安定するかなーと思いました。</p>
+      </div>
+    </article>
     
     <article class="article-1">
       <header class="header-1">
