4c29a64aa5e3fee190e5e36363b28cb40b6693b6
Nunifuchisaka <nii.splatoon@gmail.com>
2016-04-19 17:19:41 +0900

    hoge

---
+++ www/htdocs/_index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="ja" prefix="og: http://ogp.me/ns# fb: http://www.facebook.com/2008/fbml mixi: http://mixi-platform.com/ns#">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width">
+<meta name="format-detection" content="telephone=no">
+
+<!-- MEMO：イカやりたい -->
+
+<title>ほぼ日刊ぬにふちさか</title>
+<meta name="description" content="自己紹介とか日記を書いていこうと思います。">
+<meta name="keywords" content="Splatoon">
+<meta name="author" content="nii.splatoon@gmail.com">
+
+<!-- ogp -->
+<!--
+<meta property="og:site_name" content="">
+<meta property="og:title" content="">
+<meta property="og:description" content="">
+<meta property="og:url" content="">
+<meta property="og:type" content="">
+<meta property="og:image" content="">
+-->
+
+<!-- stylesheet -->
+<link rel="stylesheet" href="/assets/css/common.css?v=2">
+<link rel="stylesheet" href="/assets/css/utility.css">
+<link rel="stylesheet" href="/assets/css/patch.css">
+
+<noscript>
+<link rel="stylesheet" href="/assets/css/noscript.css">
+</noscript>
+
+<!--[if lte IE 8]>
+<script type="text/javascript" src="/assets/lib/html5shiv.js"></script>
+<![endif]-->
+
+<!-- analytics -->
+<script src="/assets/js/analytics.js"></script>
+</head>
+<body id="top" class="body--index">
+
+<?php
+require_once('assets/html/facebook-sdk.php');
+require_once('assets/html/header.php');
+?>
+
+
+<header class="l-header-1">
+  <h1 class="l-header-1__h1">
+    <a href="/">ほぼ日刊ぬにふちさか</a>
+  </h1>
+</header>
+
+
+<div class="l-container-1">
+  <div class="l-container-1__primary">
+    
+    <article class="article-1">
+      <header class="header-1">
+        <h1 class="header-1__h1">キャプチャボードなるものを購入いたしました</h1>
+        <p class="header-1__date">2015.11.22</p>
+      </header>
+      <div class="article-1__contents">
+        <p>購入いたしましたのは <strong>Elgato Game Capture HD60</strong> というもの。こちら設定とか管理画面とかものすんごく簡単で大変満足しているのですが、ゲームをプレイするほうのテレビに0.1秒ほどの<strong>遅延</strong>・<strong>ラグ</strong>があるので、もしご購入を考えている方はご注意ください…。<strong>パススルー</strong>とはいったいなんだったのか…(´・ω・`) HDMI分配器を買い足すか…。</p>
+        <p>ということで数戦してだんだん慣れてきましたので、ガチマッチを録画してみました。<br />ひとつめ：<a href="https://youtu.be/HkWRJkAVxPY">https://youtu.be/HkWRJkAVxPY</a><br />ふたつめ：<a href="https://youtu.be/WCUo0Re0844">https://youtu.be/WCUo0Re0844</a></p>
+        <div class="movie-1">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/HkWRJkAVxPY" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="movie-1">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/WCUo0Re0844" frameborder="0" allowfullscreen></iframe>
+        </div>
+      </div>
+    </article>
+    
+    <article class="article-1">
+      <header class="header-1">
+        <h1 class="header-1__h1">タッグマッチ</h1>
+        <p class="header-1__date">2015.11.18</p>
+      </header>
+      <div class="article-1__contents">
+        <p>今日はしろーさん、きーな。さん、ひいらさんとタッグマッチをしました(*´ω｀*)<br />ヒラメとキンメダイのホコだったので苦手ステージだったけど、最後の20分くらいはそこそこ動けたかなと思います。やっぱり僕は慎重すぎるのはだめだなーと思いました。ホコ運びで2回貢献できたのが嬉しかったー！(>ω<)<br />みんながっつり守ってくれたので僕は運んでるだけだったけど！(*´∀｀*)<br />そしてふたりともフレンドになってくれてとっても嬉しいです(´；ω；｀)</p>
+        <p>その後にゆきさんに合流してミルねーさんとまたたびさんもいて今日も楽しいガチナワバリができました。特にゆきさんが敵のことが多くて、めっちゃ狙…積極的に会いに行ったのでガチンコな撃ち合いがたくさんできてすごく楽しかったー(*´∀｀*) 今日はだいぶ満足して寝れそうです。おやすみなさい( ˘ω˘)ｽﾔｧ</p>
+        <figure>
+          <img src="img/20151118-1.jpeg" alt="" width="520" height="330">
+          <figcaption>しろーさんといっしょ</figcaption>
+        </figure>
+        <figure>
+          <img src="img/20151118-2.jpeg" alt="" width="520" height="330">
+          <figcaption>きーな。さんといっしょ</figcaption>
+        </figure>
+        <figure>
+          <img src="img/20151118-3.jpeg" alt="" width="520" height="330">
+          <figcaption>ひいらさんといっしょ</figcaption>
+        </figure>
+        <figure>
+          <img src="img/20151118-4.jpg" alt="" width="1024" height="576">
+          <figcaption>きーな。さんが撮ってくれたぬにふちさか。格好いい！ありがとうございます(>ω<)</figcaption>
+        </figure>
+      </div>
+    </article>
+    
+    <article class="article-1">
+      <header class="header-1">
+        <h1 class="header-1__h1">ガチナワバリ</h1>
+        <p class="header-1__date">2015.11.16</p>
+      </header>
+      <div class="article-1__contents">
+        <p>今日はけろさん、せつなさん、ひいらさんとキンメダイを散歩したり、3対1をやったりすごく楽しかった！そのあとすぐフレンドさんだけのガチナワバリが始まって、激アツですごく楽しかった！ひさしぶりに.52ガロンで活き活きと動けたかな〜♪最近キンメダイで負けまくったから今S76だけど、絶不調でも頑張って奮闘し続けた分、少しは強くなれたかな〜？いや、モンガラが動きやすいだけかな〜。</p>
+        <p>ダイナモはほんと横の判定がすごーく薄くなっちゃって「え！？今殴ったよね！？」ってことがあってびっくりしたけど、真正面捉えればちょっと距離あっても持っていけるから強いな〜。</p>
+        <p>早く動画を撮れる環境を作りたいな〜。…文章にまとまりがないｗゴメンナサイ！</p>
+        <figure>
+          <img src="img/20151116-1.jpeg" alt="" width="800" height="450">
+          <figcaption>商店街仕事したあああああ！かわいいいいい！！<br />イヤーマフ厳選します。</figcaption>
+        </figure>
+      </div>
+    </article>
+    
+    <article class="article-1">
+      <header class="header-1">
+        <h1 class="header-1__h1">SGさんとぬにふちさか</h1>
+        <p class="header-1__date">2015.11.15</p>
+      </header>
+      <div class="article-1__contents">
+        <figure>
+          <img src="img/20151115-1.jpeg" alt="" width="520" height="330">
+          <figcaption>姉妹みたいだったので記念にパシャリ！<br />ラバーソールかわいい。</figcaption>
+        </figure>
+        <figure>
+          <img src="img/20151115-2.jpeg" alt="" width="520" height="330">
+          <figcaption>見つめ合うふたり。一体なにがはじまるのか！？</figcaption>
+        </figure>
+        <figure>
+          <img src="img/20151115-3.jpeg" alt="" width="520" height="330">
+          <figcaption>どうなってしまうのかー！？（オチはありません）</figcaption>
+        </figure>
+        <figure>
+          <img src="img/20151115-4.jpeg" alt="" width="800" height="450">
+          <figcaption>エチゼンくんありがとうおおおお！</figcaption>
+        </figure>
+      </div>
+    </article>
+    
+    <article class="article-1">
+      <header class="header-1">
+        <h1 class="header-1__h1">はじめてのキンメダイ</h1>
+        <p class="header-1__date">2015.11.14</p>
+      </header>
+      <div class="article-1__contents">
+        <p>キンメダイ美術館楽しい！<br />ひとりでガチマやってたらあまりにボロ負けするので、ゆきさんに練習タッグマッチをお付き合いいただきました٩(๑òωó๑)۶ すごく楽しかったし結構勝てた！ダイナモで塗ってセンサーしてるだけでゆきさんが倒していってくれたｗありがとうございました(≧▽≦)</p>
+        <p>キンメダイは.52ガロンよりダイナモのほうがいいかなー？って感じ。<br />スプリンクラーがめっちゃ強かった！他のステージよりも塗勝つことが大事かなー？慣れてないだけかなー？</p>
+        <figure>
+          <img src="img/20151114-1.jpeg" alt="" width="800" height="450">
+          <figcaption>ゆきさんとタッグマッチ記念ヾ(｡&#62;ω&#60;｡)ﾉﾞ✧*。</figcaption>
+        </figure>
+        <figure>
+          <img src="img/20151114-2.jpeg" alt="" width="800" height="450">
+          <figcaption>第8回フェスは海の幸派で参ります</figcaption>
+        </figure>
+      </div>
+    </article>
+    
+    <article class="article-1">
+      <header class="header-1">
+        <h1 class="header-1__h1">.52ガロンを眺めるイカちゃん</h1>
+        <p class="header-1__date">2015.11.13</p>
+      </header>
+      <div class="article-1__contents">
+        <p>くそかわ</p>
+        <figure>
+          <img src="img/20151113-1.jpeg" alt=".52ガロンを眺めるイカちゃん" width="800" height="450">
+        </figure>
+        <figure>
+          <img src="img/20151113-2.jpeg" alt=".52ガロンを眺めるイカちゃん（アップ）" width="180" height="260">
+          <figcaption>「・・・（.52ガロンかわいい）」</figcaption>
+        </figure>
+      </div>
+    </article>
+    
+    <article class="article-1">
+      <header class="header-1">
+        <h1 class="header-1__h1">ボールドマーカーネオ所感</h1>
+        <p class="header-1__date">2015.11.12</p>
+      </header>
+      <div class="article-1__contents">
+        <p>念願のボールドマーカーネオ超嬉しい。無印よりネオのほうが僕は好き。ひたすらポイセン投げまくってプレッシャーかけていく形が面白いかなと思う。</p>
+        <p>ギアはイカ速度16、インク回復9、マキガ、安全靴でS+40くらいから連戦してS90くらいで終了。基本僕もボロボロだったけど、僕のキルレが２くらいで味方が奮わず、といったことが試合も多かった。いつもの.52ガロンの立ち回りと比べて味方に負担をかけてしまっていたのかもしれない。裏取りもいつもよりやったし。正面から撃ち合えないっていうのはきついね。</p>
+        <p>ダイオウは普通に強かった。特にイカ速度乗ってるからサクっと複数杯持っていけることが多かった。逆に速すぎて空振りしまくって恥ずかしかったけど慣れれば大丈夫…！</p>
+        <p>次はギアをスペシャル減少10、イカ速度27、スペシャル増加10、安全靴で試してみたい。マキガなくて通用するかな？と思うけどスペシャルをガンガン使っていく形のほうが安定するかなーと思いました。</p>
+      </div>
+    </article>
+    
+    <article class="article-1">
+      <header class="header-1">
+        <h1 class="header-1__h1">ホームページ作りました。</h1>
+        <p class="header-1__date">2015.11.11</p>
+      </header>
+      <div class="article-1__contents">
+        <p>ちょっとずつ更新します。</p>
+      </div>
+    </article>
+    
+  </div>
+  <div class="l-container-1__secondary">
+    
+    <?php
+    require_once('assets/html/sidebar.php');
+    ?>
+    
+  </div>
+<!-- /.l-container-1 --></div>
+
+
+
+<?php
+require_once('assets/html/footer.php');
+require_once('assets/html/javascript.php');
+?>
+
+</body>
+</html>
